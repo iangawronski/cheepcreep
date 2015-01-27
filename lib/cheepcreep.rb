93c2a8f6429e29856b504985a5371943c4b3680b
Ian Gawronski <iangawronski@gmail.com>
2015-01-27 09:28:52 -0500

    still need to figure out pulling out 20 followers

--- lib/cheepcreep.rb
+++ lib/cheepcreep.rb
@@ -4,6 +4,7 @@ require "httparty"
 require "pry"
 
 module Cheepcreep
+  class CreateGithubUser < ActiveRecord::Base
 end
 
 class Github
@@ -12,15 +13,45 @@ class Github
 
   def initialize
     # ENV["FOO"] is like echo $FOO
-    #@auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
+    @auth = {:username => ENV['GITHUB_USER'], :password => ENV['GITHUB_PASS']}
   end
+
+  def get_user(screen_name, options = {})
+    options.merge!({:basic_auth => @auth})
+    result = self.class.get("/users/#{screen_name}", options)
+    json = JSON.parse(result.body)
+  end
+
+  def get_followers(screen_name, options = {})
+    options.merge!({:basic_auth => @auth})
+    result = self.class.get("/users/#{screen_name}/followers", options)
+    json = JSON.parse(result.body)
+  end
+
+  # def get_gists(screen_name)
+  #   result = self.class.get("/users/#{screen_name}/gists")
+  #   json = JSON.parse(result.body)
+  #   binding.pry
+  # end
+
 end
 
+binding.pry
 
 class CheepcreepApp
+  def store_user_information(screen_name)
+    Cheepcreep::GithubUser.create(login: screen_name["login"], name: screen_name["name"], blog: screen_name["blog"], public_repos: screen_name["public_repos"].to_i, followers: screen_name["followers"].to_i, following: screen_name["following"].to_i)
+
 end
 
 binding.pry
 
 creeper = CheepcreepApp.new
 creeper.creep
+
+
+
+github = Github.new
+resp = github.get
+
+# @auth = {username: u, :password p}
